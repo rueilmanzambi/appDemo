7e6a39b10bb19ed8fe344b4d7c403aa32ed23644
Rueil Manzambi <53281957+rueilmanzambi@users.noreply.github.com>
2023-07-15 20:17:31 -0500

    finished grocery list

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'pm-grocery',
   templateUrl: './grocery.component.html',
   styleUrls: ['./grocery.component.css']
 })
-export class GroceryComponent {
 
+
+export class GroceryComponent {
+  task!: string;
+  tasks: string[] = [];
+  isChecked: boolean = false;
+
+  constructor() {
+    //this.tasks = { name: [] };
+  }
+
+  onClick(item: string) {
+    //this.tasks.name.push(this.task);
+    if (item !== ''){
+      this.tasks.push(item);
+    }
+    console.log(this.task);
+    this.task = '';
+    console.log(this.tasks);
+  }
+  onDelete(index: number) {
+    this.tasks.splice(index, 1);
+    console.log(this.tasks);
+  }
+  
 }
